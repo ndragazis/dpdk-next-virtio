0a46582dc1d02c0dc5069347ffff1a64239385f2
Nikos Dragazis <ndragazis@arrikto.com>
2019-03-03 19:02:50 +0200

    usertools: add virtio-vhost-user devices to dpdk-devbind.py
    The virtio-vhost-user PCI adapter is not detected in any existing group
    of devices supported by dpdk-devbind.py.  Add a new "Others" group for
    miscellaneous devices like this one.
    
    Signed-off-by: Nikos Dragazis <ndragazis@arrikto.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- usertools/dpdk-devbind.py
+++ usertools/dpdk-devbind.py
@@ -30,6 +30,8 @@ cavium_zip = {'Class': '12', 'Vendor': '177d', 'Device': 'a037',
               'SVendor': None, 'SDevice': None}
 avp_vnic = {'Class': '05', 'Vendor': '1af4', 'Device': '1110',
               'SVendor': None, 'SDevice': None}
+virtio_vhost_user = {'Class': '00', 'Vendor': '1af4', 'Device': '1017,1058',
+                     'SVendor': None, 'SDevice': None}
 
 octeontx2_sso = {'Class': '08', 'Vendor': '177d', 'Device': 'a0f9,a0fa',
               'SVendor': None, 'SDevice': None}
@@ -41,6 +43,7 @@ crypto_devices = [encryption_class, intel_processor_class]
 eventdev_devices = [cavium_sso, cavium_tim, octeontx2_sso]
 mempool_devices = [cavium_fpa, octeontx2_npa]
 compress_devices = [cavium_zip]
+other_devices = [virtio_vhost_user]
 
 # global dict ethernet devices present. Dictionary indexed by PCI address.
 # Each device within this is itself a dictionary of device properties
@@ -595,6 +598,8 @@ def show_status():
     if status_dev == "compress" or status_dev == "all":
         show_device_status(compress_devices , "Compress")
 
+    if status_dev == 'other' or status_dev == 'all':
+        show_device_status(other_devices, "Other")
 
 def parse_args():
     '''Parses the command-line arguments given by the user and takes the
@@ -670,6 +675,7 @@ def do_arg_actions():
             get_device_details(eventdev_devices)
             get_device_details(mempool_devices)
             get_device_details(compress_devices)
+            get_device_details(other_devices)
         show_status()
 
 
@@ -690,6 +696,7 @@ def main():
     get_device_details(eventdev_devices)
     get_device_details(mempool_devices)
     get_device_details(compress_devices)
+    get_device_details(other_devices)
     do_arg_actions()
 
 if __name__ == "__main__":
