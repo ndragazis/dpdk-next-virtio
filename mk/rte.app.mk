c564a4a526ceadcf644d71085fffe04be6783094
Nikos Dragazis <ndragazis@arrikto.com>
2019-06-09 22:53:51 +0300

    config: add option for the virtio-vhost-user transport
    Add a configuration option for compiling and linking with the
    virtio-vhost-user library.
    
    Signed-off-by: Nikos Dragazis <ndragazis@arrikto.com>

--- mk/rte.app.mk
+++ mk/rte.app.mk
@@ -132,7 +132,7 @@ ifeq ($(CONFIG_RTE_EAL_VFIO),y)
 _LDLIBS-$(CONFIG_RTE_LIBRTE_FSLMC_BUS)      += -lrte_bus_fslmc
 endif
 
-ifeq ($(CONFIG_RTE_LIBRTE_VHOST),y)
+ifeq ($(CONFIG_RTE_LIBRTE_VHOST)$(CONFIG_RTE_LIBRTE_VIRTIO_VHOST_USER),yy)
 _LDLIBS-y += --no-as-needed
 _LDLIBS-y += -lrte_virtio_vhost_user
 _LDLIBS-y += --as-needed
